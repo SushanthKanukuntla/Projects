494bc2678d3876762297637c83d21e0f4a515338
SushanthKanukuntla <sushanth.kanukuntla@gmail.com>
2017-03-07 10:17:02 -0500

    Sample Python
    Sample Python

---
+++ Pythonfirst/src/base/first.py
@@ -0,0 +1,12 @@
+class First:
+    
+    def add(self, a,b):
+        c= a+b
+        return c
+    
+def main():
+    f = First()
+    c = f.add(2,3)
+    print c
+    
+if __name__ == '__main__':main()    
---
+++ Pythonfirst/src/first.py
@@ -0,0 +1,8 @@
+'''
+Created on 03-Mar-2017
+
+@author: susha
+'''
+
+if __name__ == '__main__':
+    pass
